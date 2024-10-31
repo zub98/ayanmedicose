9b32d11a2530611db66b868532aa7d4ec42e4199
ZUBAIR ALAM <151960284+zub98@users.noreply.github.com>
2024-10-31 21:36:52 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Services from './pages/Services';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App; 
